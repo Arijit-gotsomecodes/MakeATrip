8263c52e24115847cbc59359dbc7d7bdcf114a7e
Arijit-gotsomecodes <arijit.deb1@s.amity.edu>
2024-07-07 16:31:05 +0530

    added working user registraion

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -40,7 +40,7 @@ async (req: Request, res: Response) => {
       secure: process.env.NODE_ENV === "production",
       maxAge: 86400000,
     });
-    return res.sendStatus(200);
+    return res.status(200).send({ message: "User registered successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Something went wrong" });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout><p>Home Page</p> </Layout>} />
         <Route path="/search" element={<Layout><p>Search Page</p> </Layout>} />
+        <Route path="/register" element={<Layout><Register/></Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
